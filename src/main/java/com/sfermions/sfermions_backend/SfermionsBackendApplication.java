3e9b0980ceef55cab3eab0a198fde0f911c9c44a
su-hwani <wjdtnghks123@kookmin.ac.kr>
2024-08-06 03:17:45 +0900

    Init Project

---
+++ src/main/java/com/sfermions/sfermions_backend/SfermionsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sfermions.sfermions_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfermionsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfermionsBackendApplication.class, args);
+	}
+
+}
